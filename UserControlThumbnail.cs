6464ae931df6bf2253a5c8d4449413578593081e
mario <mario.guthrie@ipvidnet.com>
2011-04-12 16:55:25 -0700

    added junk

---
+++ UserControlThumbnail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QA.GUI
+{
+    public partial class UserControlThumbnail : UserControl
+    {
+        public UserControlThumbnail()
+        {
+            InitializeComponent();
+        }
+    }
+}
